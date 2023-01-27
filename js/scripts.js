e6481b24ff66cd2fbdfa000d91d19e7eb9ef11c0
Luca Giommoni <luca.giommoni91@gmail.com>
2023-01-27 22:52:55 +0100

    added logic to create certs

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,28 @@
-document.getElementById('currentYear').textContent = new Date().getUTCFullYear()
+//document.getElementById('currentYear').textContent = new Date().getUTCFullYear()
+
+function createNodeFromJson(jsonData) {
+  let el = document.getElementById("gridOfCertifications");
+  jsonData.certs.forEach(
+    (e) =>
+      (el.innerHTML += `<div class="col-lg-4 col-sm-6" style="border: 4px solid transparent;">
+  <a
+    class="portfolio-box"
+    href="${e.source}"
+    style="background-image: url(${e.source});"
+  >
+    <div class="portfolio-box-caption">
+      <div class="project-name">
+        ${e.name}
+      </div>
+      <div class="project-category text-white-50">
+        ${e.date}
+      </div>
+    </div>
+  </a>
+</div>`)
+  );
+}
+
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((json) => createNodeFromJson(json));
