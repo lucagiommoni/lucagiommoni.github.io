cce76e20016690ef97558bee718b78a9ff788cd3
Luca Giommoni <33961603+lucagiommoni@users.noreply.github.com>
2023-08-09 10:28:28 +0000

    skill and certifications carousel

--- contents/intro.md
+++ contents/intro.md
@@ -4,13 +4,13 @@
 
 Hello,
 
-I am Luca, a computer engineer with <span id="yow"></span> years' experience in IT companies. Over the last years, I have gained extensive experience in the field of Cloud Computing and DevOps. 
+I am Luca, a computer engineer with <span id="yow"></span> years' experience in IT companies. Over the last years, I have gained extensive technical experience in the ICT market working on Cloud Computing and DevOps, but also managing teams and big projects.
 
-I started my career in Intesa Sanpaolo, working on the Disaster Recovery project. Then, as a Cloud Architect, I designed and implemented private/hybrid/public cloud solutions, ensuring that they met the business requirements and were aligned with the company's IT strategy. 
+I started my career in **Intesa Sanpaolo**, working on the _**Disaster Recovery**_ project. Then, as a _**Cloud Architect**_, I designed and implemented cloud solutions, ensuring that they met the business requirements and were aligned with the company strategy.I was also responsible for a group of external consultants for support and operation projects. After that, I moved to Stellantis to work on _**Connected Features**_ projects. Here I initially worked as _**Feature Design Architect**_, then covering the role of _**Program Manager**_. Hence, I managed connectivity programs, with a particular emphasis on _**xOTA**_ (updates over-the-air) projects. During my time at Stellantis, I gained valuable experience in managing complex projects that involved multiple stakeholders, external providers and big teams. I learned how to coordinate teams and to ensure projects were delivered on time and within budget. Currently, I am working as a _**Cloud Migration Coordinator**_ and _**L3 Application Support Specialist**_ at **MSC Technology**. I am responsible for the migration of the company's IT infrastructure to the **Azure** cloud platform and leading a 10-person team for the L3 application support management. In MSC, I have demonstrated my ability to work with cross-functional teams, manage multiple projects simultaneously, and drive results through effective leadership and collaboration. I also enforced valuable experience on Cloud technology and DevOps processes while gaining more management skills. 
 
-After that, I worked as Connected Feature Architect and Program Manager at Stellantis, where at the beginning was responsible for designing the new connected features that would eventually be implemented on new vehicles. Afterwards, I began managing connectivity programs, with a particular emphasis on xOTA (updates over-the-air) projects. During my time at Stellantis, I gained valuable experience in DevOps processes and in managing complex projects involving multiple stakeholders, coordinating resources and timelines, and ensuring that projects were delivered on time and within budget.
+Thanks for surfing my profile
 
-Currently, I am working as a Cloud Coordinator and L3 Application Support Specialist at MSC Technology, where I am responsible for the migration of the company's IT infrastructure to the Azure cloud platform and leading a 10 people team for the L3 application support management. In MSC, I have demonstrated my ability to work with cross-functional teams, manage multiple projects simultaneously, and drive results through effective leadership and collaboration. I also enforced valuable experience on Cloud technology and DevOps processes.
-I am confident that my experience and skills make me a strong fit for this role. I am excited about the prospect of working for an innovative company that values modern technologies and approaches to work.
+Sincerely,
+Luca
 
 <br><br>
--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
+document.getElementById("currentYear").innerText = new Date().getUTCFullYear();
+
+/*******************************************************************/
+
 const contentUrls = [
   "contents/intro.md",
   "contents/academic.md",
   "contents/workExp.md"
 ]
 
-document.getElementById("currentYear").innerText = new Date().getUTCFullYear();
+const calls = contentUrls.map(url => fetch(url).then((res) => res.text()))
+
+Promise.all(calls)
+  .then(results => fillinSection(results))
+  .finally(_ => document.getElementById("yow").innerHTML = (new Date().getFullYear() - 2016))
 
 function fillinSection(txt) {
   if (typeof txt === "object" && Array.isArray(txt)) {
@@ -18,6 +26,39 @@ function fillinSection(txt) {
   }
 }
 
+/*******************************************************************/
+
+fetch("data.json")
+  .then((response) => response.json())
+  .then((json) => createNodeFromJson(json));
+
+function createNodeFromJson(jsonData) {
+
+  // Build Companies
+
+  const gridOfCompanies = document.getElementById("gridOfCompanies");
+  jsonData.companies.forEach(e => {
+    gridOfCompanies.innerHTML += `
+<div class="col-12 col-md-3 text-center">
+  <a href="${e.src}" target="_blank" rel="nofollow noopener noreferrer">
+    <img src="${e.img}" alt="${e.name}" style="width: 15em" />
+  </a>
+</div>`
+  })
+
+  // Build Certifications Carousel
+
+  const carousel = document.querySelector('#certImgCarousel_01 .carousel-inner')
+  jsonData.certifications.forEach((cert,i) => {
+    const active = i === 0 ? 'active' : ''
+    carousel.innerHTML += `
+<div class="carousel-item ${active}">
+  <img src="${cert.source}" class="d-block w-100" alt="${cert.name}">
+</div>
+`
+  })
+}
+
 function enforceHyperLink(html) {
   const links = html.getElementsByTagName('a')
   if (links.length !== 0) {
@@ -28,55 +69,3 @@ function enforceHyperLink(html) {
   }
 }
 
-function createNodeFromJson(jsonData) {
-  buildCompaniesElement(jsonData.companies);
-  buildSkillsElement(jsonData.certifications);
-  // buildSkillsElement(jsonData.certifications.sort((a, b) => (new Date(b.date) - new Date(a.date))));
-}
-
-function buildCompaniesElement(companies) {
-  // console.log(companies);
-  let el = document.getElementById("gridOfCompanies");
-  companies.forEach(
-    (e) =>
-    (el.innerHTML += `
-    <div class="col-12 col-md-3 text-center">
-      <a href="${e.src}" target="_blank" rel="nofollow noopener noreferrer">
-        <img src="${e.img}" alt="${e.name}" style="width: 15em" />
-      </a>
-    </div>`)
-  );
-}
-
-function buildSkillsElement(certs) {
-  let skills = []
-  certs.forEach(e => { skills = [...skills, ...e.skills] })
-  skills = skills.filter((e, i) => skills.indexOf(e) === i)
-  console.log(skills);
-  const parentDiv = document.getElementById("skillsGrid");
-  skills.forEach(skill => {
-    parentDiv.innerHTML += `<div class="px-2"><div class="w-100 badge bg-light text-secondary py-3">${skill}</div></div>`
-  })
-  //   certs.forEach(
-  //     (e) =>
-  //     (el.innerHTML += `
-  //     <div class="col-lg-4 col-sm-6" style="border: 4px solid transparent;">
-  //       <a class="portfolio-box" href="${e.source}" style="background-image: url(${e.source});">
-  //       <div class="portfolio-box-caption">
-  //       <div class="project-name">${e.name}</div>
-  //       <div class="project-category text-white-50">${e.date}</div>
-  //     </div>
-  //   </a>
-  // </div>`)
-  //   );
-}
-
-fetch("data.json")
-  .then((response) => response.json())
-  .then((json) => createNodeFromJson(json));
-
-const calls = contentUrls.map(url => fetch(url).then((res) => res.text()))
-
-Promise.all(calls)
-  .then(results => fillinSection(results))
-  .finally(_ => document.getElementById("yow").innerHTML = (new Date().getFullYear() - 2016))
